17f76973d3f78d86af5c2f295400beb8a0a19a1d
Simo Kinnunen <simo@shoqolate.com>
2014-11-13 21:16:21 +0900

    Update PIE switcher. No need to compile twice. No need to have different run scripts for PIE/non-PIE versions either.

--- jni/minitouch/Android.mk
+++ jni/minitouch/Android.mk
@@ -2,16 +2,25 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := minitouch-common
+
+LOCAL_SRC_FILES := \
+	minitouch.c \
+
+LOCAL_STATIC_LIBRARIES := \
+	libevdev \
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 # Enable PIE manually. Will get reset on $(CLEAR_VARS).
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
 
 LOCAL_MODULE := minitouch
 
-LOCAL_SRC_FILES := \
-	minitouch.c \
-
-LOCAL_STATIC_LIBRARIES := libevdev
+LOCAL_STATIC_LIBRARIES := minitouch-common
 
 include $(BUILD_EXECUTABLE)
 
@@ -19,9 +28,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := minitouch-nopie
 
-LOCAL_SRC_FILES := \
-	minitouch.c \
-
-LOCAL_STATIC_LIBRARIES := libevdev
+LOCAL_STATIC_LIBRARIES := minitouch-common
 
 include $(BUILD_EXECUTABLE)
--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
+
+# Fail on error, verbose output
 set -exo pipefail
-ndk-build
+
+# Build project
+ndk-build 1>&2
+
+# Figure out which ABI and SDK the device has
 abi=$(adb shell getprop ro.product.cpu.abi | tr -d '\r')
-adb push libs/$abi/minitouch /data/local/tmp/
-adb shell /data/local/tmp/minitouch "$@"
+sdk=$(adb shell getprop ro.build.version.sdk | tr -d '\r')
+
+# PIE is only supported since SDK 16
+if (($sdk >= 16)); then
+  bin=minitouch
+else
+  bin=minitouch-nopie
+fi
+
+# Upload the binary
+adb push libs/$abi/$bin /data/local/tmp/
+
+# Run!
+adb shell /data/local/tmp/$bin "$@"
